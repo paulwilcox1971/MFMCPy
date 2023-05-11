88160f3a3c1719263fdb01e897e3768ebf3cf70d
paulwilcox1971 <p.wilcox@bristol.ac.uk>
2023-05-11 01:43:45 +0100

    Rationalised files and Started viewer

---
+++ MFMC_Py.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 10 22:24:20 2023
+
+@author: mepdw
+"""
+
+#functions for public access imported here so that they are all accessible
+#from same namespace - should be 'better' way to do this?
+from MFMC_checker import fn_load_specification, fn_check_sequence
+
+from MFMC_viewer import cl_sequence
+
+from MFMC_utils import fn_open_file, fn_close_file, fn_get_sequence_list, fn_transmit_laws_for_sequence, fn_receive_laws_for_sequence
---
+++ MFMC_viewer.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 10 23:31:56 2023
+
+@author: mepdw
+"""
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import MFMC_utils
+
+class cl_sequence:
+    tx_laws = []
+    rx_laws = [] 
+    unique_tx_laws = []
+    unique_rx_laws = []
+    time = []
+    axes_defined = False
+    
+    def __init__(self, MFMC, sequence):
+        self.tx_laws = MFMC_utils.fn_transmit_laws_for_sequence(MFMC, sequence)
+        self.rx_laws = MFMC_utils.fn_receive_laws_for_sequence(MFMC, sequence)
+        self.unique_tx_laws = list(set(self.tx_laws))
+        self.unique_rx_laws = list(set(self.rx_laws))
+        self.tx = [self.unique_tx_laws.index(i) for i in self.tx_laws]
+        self.rx = [self.unique_rx_laws.index(i) for i in self.rx_laws]
+        self.time = MFMC[sequence].attrs['TIME_STEP'] * range(MFMC[sequence]['MFMC_DATA'].shape[2]) + MFMC[sequence].attrs['START_TIME']
+        self.axes_defined = True
+        self.mfmc_data = MFMC[sequence]['MFMC_DATA']
+    
+    def fn_return_frame(self, frame_no = 0, tx = None, rx = None, tmin = -np.inf, tmax = np.inf):
+        if not self.axes_defined:
+            return None
+        else:
+            
+            if tx != None:
+                tx_rx_i = np.where(np.array(self.tx) == tx)[0]
+                ascan_label =  [self.rx[i] for i in tx_rx_i]
+            elif rx != None:
+                tx_rx_i = np.where(np.array(self.rx) == rx)[0]
+                ascan_label =  [self.tx[i] for i in tx_rx_i]
+            else:
+                tx_rx_i = list(range(self.mfmc_data.shape[1]))
+                ascan_label = tx_rx_i
+            #time_i = [t >= tmin and t <= tmax for t in self.time]
+            t1 = np.min(np.where(self.time >= tmin))
+            t2 = np.max(np.where(self.time <= tmax)) + 1
+            return (self.time[t1:t2], ascan_label, self.mfmc_data[frame_no, tx_rx_i, t1:t2])
+        
+
