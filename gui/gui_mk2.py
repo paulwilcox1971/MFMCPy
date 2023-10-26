93675be51477d6860acb720ba6a62de28a9b5610
paulwilcox1971 <p.wilcox@bristol.ac.uk>
2023-10-26 23:50:54 +0100

    Improved probe plotting

--- gui/gui_mk2.py
+++ gui/gui_mk2.py
@@ -11,8 +11,10 @@ from tkinter.messagebox import showinfo
 
 from matplotlib.figure import Figure 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg,  NavigationToolbar2Tk
-from matplotlib.patches import Rectangle
+from matplotlib.patches import Rectangle, Ellipse, Wedge
 from matplotlib.collections import PatchCollection
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Patch3DCollection
 
 #Set working directory one level up from where this file is
 path = os.path.abspath(__file__)
@@ -21,7 +23,7 @@ os.chdir(os.sep.join(dir_path.split(os.sep)[0:-1]))
 import mfmc as m
 
 fname = os.sep.join(['example MFMC files', 'unit-test test file.mfmc'])
-fname = os.sep.join(['example MFMC files', 'some probes2.mfmc'])
+fname = os.sep.join(['example MFMC files', 'some probes3.mfmc'])
 
 
 class cl_mfmc_explorer:
@@ -69,6 +71,8 @@ class cl_mfmc_explorer:
         
         self.fig = Figure(figsize = (5, 5), dpi = 100)
         self.ax = self.fig.add_subplot(111)
+        # self.ax = self.fig.add_subplot(111, projection='3d')
+        
         self.canvas = FigureCanvasTkAgg(self.fig,  master = self.root)
         self.canvas.get_tk_widget().grid(column = 3, row = 0, rowspan = 2, sticky = 'nsew', padx=5, pady=5)
         
@@ -211,24 +215,56 @@ def select_file():
     )
 
 def fn_plot_probe(ax, probe):
-    xc, yc, zc = probe[m.strs.h5_keys.ELEMENT_POSITION].T
+    x, y, z = probe[m.strs.h5_keys.ELEMENT_POSITION].T
     e1x, e1y, e1z = probe[m.strs.h5_keys.ELEMENT_MAJOR].T
     e2x, e2y, e2z = probe[m.strs.h5_keys.ELEMENT_MINOR].T
+    typ = probe[m.strs.h5_keys.ELEMENT_SHAPE].T
     
     w1 = np.sqrt(e1x ** 2 + e1y ** 2 )
     w2 = np.sqrt(e2x ** 2 + e2y ** 2 )
-    theta = np.arctan2(e1y, e2y)
+    theta = np.arctan2(e1y, e1x)
     
     ax.clear()
     ax.axis('equal')
     
-    elements = [Rectangle((x - ww1 / 2, y - ww2 / 2), ww1, ww2, angle = t * 180 / np.pi, rotation_point = 'center')
-                  for x, y, ww1, ww2, t in zip(xc, yc, w1, w2, theta)]
+    # elements = [Rectangle((x - ww1, y - ww2), ww1 * 2, ww2 * 2, angle = t * 180 / np.pi, rotation_point = 'center')
+    #               for x, y, ww1, ww2, t in zip(xc, yc, w1, w2, theta)]
+    elements= []
+    #for x, y, ww1, ww2, t, tp in zip(x, yc, w1, w2, theta, typ):
+    for xx, yy, e1xx, e2xx, e1yy, e2yy, tp in zip (x, y, e1x, e2x, e1y, e2y, typ):
+        wx = np.sqrt(e1xx ** 2 + e1yy ** 2 )
+        wy = np.sqrt(e2xx ** 2 + e2yy ** 2)
+        an = np.arctan2(e1yy, e1xx) * 180 / np.pi
+        if tp == 2: #ellipse
+            elements.append(
+                Ellipse((xx, yy), wx * 2,  wy * 2, angle = an)
+                )
+        elif tp == 3: #annular 
+            rc = np.sqrt(xx ** 2 + yy ** 2)
+            if rc > 0:
+                tc = np.arctan2(yy, xx)
+                t1 = (tc - np.abs(wy) / (2 * np.pi * rc)) * 180 / np.pi
+                t2 = (tc + np.abs(wy) / (2 * np.pi * rc)) * 180 / np.pi
+                elements.append(
+                    Wedge((0.0, 0.0), rc + wx, theta1 = t1, theta2 = t2, width = 2 * wx)
+                    )
+            else:
+                elements.append(
+                    Ellipse((0.0, 0.0), wx * 2,  wy * 2, angle = an)
+                    )
+
+        else: #reactangle
+            elements.append(
+                Rectangle((xx - wx, yy - wy),  wx * 2,  wy * 2, angle = an, rotation_point = 'center')
+                )
+                
+            
     pc = PatchCollection(elements, facecolor='r',
-                     edgecolor='none', alpha=0.5)
-    #Rectangle(xy, width, height, *, angle=0.0, rotation_point='xy', **kwargs)[source]
+                      edgecolor='none', alpha=0.5)
     ax.add_collection(pc)
-    ax.plot(xc, yc, 'k.')     
+    ax.plot(x, y, 'k.')
+    ax.plot(x + e1x, y + e1y, 'r.')
+    ax.plot(x + e2x, y + e2y, 'g.')
 
 #open_button.pack(expand=True)
 
